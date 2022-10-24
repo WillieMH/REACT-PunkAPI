e7b2b173e0d26fcbdc0713ae3714979c7e4c1d53
Willie Henderson <wm2727@ou.ac.uk>
2022-10-24 20:51:43 +0100

    API Works

--- punk-api/src/App.jsx
+++ punk-api/src/App.jsx
@@ -1,19 +1,32 @@
 import './App.scss';
-import beersData from "./data/beersData.jsx"
+// import beersData from "./data/beersData.jsx"
+import { useState } from "react";
 
 import BeerTiles from "./containers/BeerTiles.jsx"
+import Button from "./components/Button/Button.jsx"
 
 const App = () => {
+  const [user, setUser] = useState([]);
+
+  const getUser = async () => {
+    const url = `https://api.punkapi.com/v2/beers`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setUser(data);
+    console.log(user)
+};
+
   return (
     <div className="container">
 
       <div className="container__lh-nav">
         <h2>NAV BAR</h2>
+        <Button onClick={getUser} label="Line Up The Bar!" />
         <h2>Items</h2>
       </div>
 
       <div className="container__beer-tiles">
-        <BeerTiles beersArry={beersData}/>
+        <BeerTiles beersArry={user}/>
       </div>
 
     </div>
--- punk-api/src/containers/BeerTiles.jsx
+++ punk-api/src/containers/BeerTiles.jsx
@@ -1,14 +1,14 @@
 import "./BeerTiles.scss"
 
 const BeerTiles = (props) => {
-  const { beersArry } = props;
+  const {beersArry} = props;
 
   const displayBeers = beersArry.map((beer) => (
 
     <div className="container__beer-tiles--beercard">
       <h2>{beer.name}</h2>
       <h4>"{beer.tagline}"</h4>
-      <img src={beer.image_url} alt="Beer" />
+      {/* <img src={beer.image_url} alt="Beer" /> */}
     </div>  
       
 
