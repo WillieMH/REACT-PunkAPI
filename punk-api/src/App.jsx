cdfd8e8d7d91c4ec3b3f97dc4625514428d758d2
Willie Henderson <wm2727@ou.ac.uk>
2022-10-27 15:23:48 +0100

    all filter options removed

--- punk-api/src/App.jsx
+++ punk-api/src/App.jsx
@@ -6,32 +6,20 @@ import SearchBar from './containers/SearchBar/SearchBar';
 
 const App = () => {
   const [punks, setPunks] = useState([]);
-  const [filterBy, setFilterBy] = useState("")
 
 
 
-  const getPunks = async (beerFilter) => {
-    let APIurl = `https://api.punkapi.com/v2/beers?per_page=80`;
-    
-    if (beerFilter === "abv > 6%") {
-      APIurl = "https://api.punkapi.com/v2/beers?per_page=80&abv_gt=6"
-    } else if (beerFilter === "classic (brewed before 2010)") {
-      APIurl = "https://api.punkapi.com/v2/beers?per_page=80&brewed_before=01-2010"
-    }
-
-    const res = await fetch(APIurl);
+  const getPunks = async () => {
+    const res = await fetch(`https://api.punkapi.com/v2/beers?per_page=80`);
     const data = await res.json();
     setPunks(data);
     console.log(punks)
 };
 
   useEffect(() => {
-    getPunks(filterBy);
-  },[filterBy]);
+    getPunks();
+  },[]);
 
-  const handleClick = (event) => {
-    setFilterBy(event.target.value);
-  };
 
 
   return (
@@ -39,13 +27,7 @@ const App = () => {
         
       <div className="navbar">
         <h1>Whats your poison....</h1>
-        <Nav
-        onChange={handleClick}
-        selected={filterBy}
-        options={["ABV > 6%", "Classic (Brewed before 2010)", "Acidic (pH > 4)"]}
-        label="Filter the beer some more..."
-        
-      />
+
         <h4>Beers returned = {punks.length}</h4>
       </div>
 
