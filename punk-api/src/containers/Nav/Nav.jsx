5fdccbc53c3bc55ae9b3b3a35033fffe2d9ef00b
Willie Henderson <wm2727@ou.ac.uk>
2022-10-27 11:51:48 +0100

    thu commit

--- punk-api/src/containers/Nav/Nav.jsx
+++ punk-api/src/containers/Nav/Nav.jsx
@@ -2,21 +2,21 @@ import "./Nav.scss";
 
 const Nav = ({ onChange, selected, options, label }) => {
   return (
-    <div className="radio-buttons">
+    <div className="check-boxes">
       <p>{label}</p>
       {options.map((option, index) => {
         const optionLower = option.toLowerCase();
 
         return (
-          <div key={"radio-button" + option + index}>
+          <div key={"check-boxes" + option + index}>
             <input
-              type="radio"
+              type="checkbox"
               name="gender"
               value={optionLower}
               checked={optionLower === selected.toLowerCase()}
               onChange={onChange}
             />
-            <label className="radio-buttons__label" htmlFor={optionLower}>
+            <label className="check-boxes__label" htmlFor={optionLower}>
               {option}
             </label>
           </div>
